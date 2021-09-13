a7142a76c085fb1d9c81a6e3e7c5aa6e1e8034d8
Benjamin Kästner <benjamin.kaestner@gmail.com>
2021-09-14 00:03:09 +0200

    Add usage example

---
+++ example/bar.h
@@ -0,0 +1,6 @@
+#ifndef LIBINFO_TEST_BAR
+#define LIBINFO_TEST_BAR
+#include "foo.h"
+
+int bar();
+#endif // LIBINFO_TEST_BAR
---
+++ example/foo.h
@@ -0,0 +1,4 @@
+#ifndef LIBINFO_TEST_FOO
+#define LIBINFO_TEST_FOO
+int foo();
+#endif // LIBINFO_TEST_FOO
